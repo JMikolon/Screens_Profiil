6d027c0fbc13101f2918ca5f6629d00061ad80ea
JMikolon <janmikolon@yahoo.de>
2017-04-13 20:51:57 +0200

    Profil
    test

---
+++ app/src/main/java/com/example/miko/screens_profiil/Profil_Screen.java
@@ -0,0 +1,13 @@
+package com.example.miko.screens_profiil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profil_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil__screen);
+    }
+}
